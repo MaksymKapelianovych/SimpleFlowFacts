1d16c035b7536234ca8123d586092fe9fec0d6ca
MaksymKapelianovych <m.kapelianovych@gmail.com>
2024-12-30 16:32:04 +0200

    Removed deprecated UFlowNode_FactCondition. Changed GetNodeTitle in UFlowNodeAddOn_PredicateFact to display custom text only when Condition is valid and adaptive titles are checked.

--- Source/SimpleFlowFacts/Private/FlowNodeAddOn_PredicateFact.cpp
+++ Source/SimpleFlowFacts/Private/FlowNodeAddOn_PredicateFact.cpp
@@ -5,6 +5,7 @@
 
 #include "FactSubsystem.h"
 #include "FactTypes.h"
+#include "FlowSettings.h"
 
 UFlowNodeAddOn_PredicateFact::UFlowNodeAddOn_PredicateFact( const FObjectInitializer& ObjectInitializer )
 	: Super( ObjectInitializer )
@@ -32,47 +33,11 @@ bool UFlowNodeAddOn_PredicateFact::EvaluatePredicate_Implementation() const
 #if WITH_EDITOR
 FText UFlowNodeAddOn_PredicateFact::GetNodeTitle() const
 {
-	return Condition.IsValid() ? FText::FromString( Condition.ToString() ) : FText::FromString( "Fact is not set" );
-}
-#endif
-
-UFlowNode_FactCondition::UFlowNode_FactCondition(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-#if WITH_EDITOR
-	NodeStyle = EFlowNodeStyle::Custom;
-	NodeColor = FLinearColor{ 0.275f, 1.f, .8f };
-	Category = TEXT( "Fact" );
-#endif
+	if ( Condition.IsValid() && UFlowSettings::Get()->bUseAdaptiveNodeTitles )
+	{
+		return FText::FromString( Condition.ToString() );
+	}
 	
-	InputPins = { FFlowPin( TEXT( "In" ) ) };
-	OutputPins = { FFlowPin( TEXT( "True" ) ), FFlowPin( TEXT( "False" ) ) };
-}
-
-void UFlowNode_FactCondition::ExecuteInput(const FName& PinName)
-{
-	if ( UWorld* World = GetWorld() )
-	{
-		UFactSubsystem& Subsystem = UFactSubsystem::Get( World );
-		TriggerOutput( Subsystem.CheckFactCondition( Condition ) ? TEXT( "True" ) : TEXT( "False" ), true );
-	}
-	else
-	{
-		LogError( TEXT( "No valid world" ) );
-		// Is it okay to trigger output in error case?
-		TriggerOutput( TEXT( "False" ), true );
-	}
-}
-
-#if WITH_EDITOR
-EDataValidationResult UFlowNode_FactCondition::ValidateNode()
-{
-	return Condition.IsValid() ? EDataValidationResult::Valid : EDataValidationResult::Invalid;
-}
-
-FString UFlowNode_FactCondition::GetNodeDescription() const
-{
-	FString Description = Condition.ToString();
-	return Description.IsEmpty() ? "None" : Description;
+	return Super::GetNodeTitle();
 }
 #endif
--- Source/SimpleFlowFacts/Public/FlowNodeAddOn_PredicateFact.h
+++ Source/SimpleFlowFacts/Public/FlowNodeAddOn_PredicateFact.h
@@ -27,24 +27,3 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Fact", meta = (ShowOnlyInnerProperties))
 	FSimpleFactCondition Condition;
 };
-
-/**
- * Node for checking fact conditions.
- * After merging https://github.com/MothCocoon/FlowGraph/pull/202 this node will be refactored.
- */
-UCLASS( meta = ( DisplayName = "Fact Condition (DEPRECATED)", Tooltip = "This node is deprecated and will be removed soon, please, use AddOn predicate" ) )
-class SIMPLEFLOWFACTS_API UFlowNode_FactCondition : public UFlowNode
-{
-	GENERATED_UCLASS_BODY()
-protected:
-	virtual void ExecuteInput( const FName& PinName ) override;
-	
-#if WITH_EDITOR
-	virtual EDataValidationResult ValidateNode() override;
-	virtual FString GetNodeDescription() const override;
-#endif
-
-protected:
-	UPROPERTY(EditAnywhere, Category = "Fact", meta = (ShowOnlyInnerProperties))
-	FFactCondition Condition;
-};
